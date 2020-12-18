ddf692f225108b523b44a099c733c16d4ac8fd87
Changkun Ou <hi@changkun.de>
2020-12-18 20:14:15 +0100

    proto: update

--- internal/types/proto/gen.go
+++ internal/types/proto/gen.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a GPL-3.0
 // license that can be found in the LICENSE file.
 
-//go:generate protoc --go_out=plugins=grpc:. midgard.proto
+//go:generate protoc --go_out=. --go-grpc_out=. midgard.proto
 
 // Package proto defines the midgard gRPC protocols.
 package proto
